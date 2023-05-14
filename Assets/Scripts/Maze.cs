e98b237849f7f214484ba7831925db47e8beb6ad
Anastasiia <anastasiia@MacBook-Pro-Anastasiia.local>
2023-05-14 13:45:34 +0800

    Added new map layer with characters and nodes positions. Added method for placing characters and nodes - Maze.PlaceCharacters

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -9,53 +9,87 @@ public class Maze : MonoBehaviour
     [SerializeField] private GameObject playerMemory;
     [SerializeField] private GameObject ghost1Memory;
     [SerializeField] private GameObject ghost2Memory;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject ghost1;
+    [SerializeField] private GameObject ghost2;
+    [SerializeField] private GameObject person;
+    [SerializeField] private GameObject node;
+
     // [SerializeField] private GameObject node;
 
     private float _startPosX;
     private float _startPosY;
-    private string _mazeData;
+    private string _mazeInactiveData;
+    private string _mazeActiveData;
 
     private void Awake()
     {
         Parse();
+        DrawElements();
+        PlaceCharacters();
     }
 
     private void Parse()
     {
         Settings.WallSize = wallBlock.transform.localScale.x;
 
-        _mazeData = File.ReadAllText(Settings.MazeMapFileName);
-        int curMazeWidth = _mazeData.IndexOf('\n');
-        int curMazeHeight = _mazeData.Length / curMazeWidth;
+        _mazeInactiveData = File.ReadAllText(Settings.MazeMapInactiveFileName);
+        _mazeActiveData = File.ReadAllText(Settings.MazeMapActiveFileName);
+        
+        int curMazeWidth = _mazeInactiveData.IndexOf('\n');
+        int curMazeHeight = _mazeInactiveData.Length / curMazeWidth;
         if (curMazeWidth != Settings.MazeWidth || curMazeHeight != Settings.MazeHeight 
-            || _mazeData.Length != Settings.MazeWidth * Settings.MazeHeight + Settings.MazeHeight - 1)
-            throw new Exception("Wrong number of rows or columns in the " + Settings.MazeMapFileName);
-
+            || _mazeInactiveData.Length != Settings.MazeWidth * Settings.MazeHeight + Settings.MazeHeight - 1)
+            throw new Exception("Wrong number of rows or columns in the " + Settings.MazeMapInactiveFileName);
 
         _startPosX = -(float)Settings.MazeWidth / 2f * Settings.WallSize;
         _startPosY = Settings.MazeHeight / 2f * Settings.WallSize;
+    }
 
+    private void DrawElements()
+    {
         for (int y = 0; y < Settings.MazeHeight; y++)
         {
             for (int x = 0; x < Settings.MazeWidth; x++)
-                DrawElements(x, y);
+            {
+                int objPosition = y * Settings.MazeWidth + x + y;
+                if (objPosition >= _mazeInactiveData.Length || _mazeInactiveData[objPosition] == '0') continue;
+
+                var expectedObj = _mazeInactiveData[objPosition] switch
+                {
+                    '1' => wallBlock.transform,
+                    '2' => playerMemory.transform,
+                    '3' => ghost1Memory.transform,
+                    '4' => ghost2Memory.transform,
+                    _ => throw new Exception("Not allowed symbols in " + Settings.MazeMapInactiveFileName)
+                };
+                var curObj = Instantiate(expectedObj, gameObject.transform, false);
+                curObj.position = new Vector3(_startPosX + x * Settings.WallSize, _startPosY - y * Settings.WallSize, 0);
+            }
         }
     }
-
-    private void DrawElements(int x, int y)
+    
+    private void PlaceCharacters()
     {
-        int objPosition = y * Settings.MazeWidth + x + y;
-        if (objPosition >= _mazeData.Length || _mazeData[objPosition] == '0') return;
-        
-        var expectedObj = _mazeData[objPosition] switch
+        for (int y = 0; y < Settings.MazeHeight; y++)
         {
-            '1' => wallBlock.transform,
-            '2' => playerMemory.transform,
-            '3' => ghost1Memory.transform,
-            '4' => ghost2Memory.transform,
-            _ => throw new Exception("Not allowed symbols in " + Settings.MazeMapFileName)
-        };
-        var curObj = Instantiate(expectedObj, gameObject.transform, false);
-        curObj.position = new Vector3(_startPosX + x * Settings.WallSize, _startPosY - y * Settings.WallSize, 0);
+            for (int x = 0; x < Settings.MazeWidth; x++)
+            {
+                int objPosition = y * Settings.MazeWidth + x + y;
+                if (objPosition >= _mazeActiveData.Length || _mazeActiveData[objPosition] == '0') continue;
+
+                var curObj = _mazeActiveData[objPosition] switch
+                {
+                    '*' => Instantiate(node.transform, gameObject.transform, false),
+                    '5' => player.transform,
+                    '6' => ghost1.transform,
+                    '7' => ghost2.transform,
+                    _ => null
+                };
+
+                if (curObj)
+                    curObj.position = new Vector3(_startPosX + x * Settings.WallSize, _startPosY - y * Settings.WallSize, 0);
+            }
+        }
     }
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -2,7 +2,8 @@ using System;
 
 public static class Settings
 {
-    public const string MazeMapFileName = "Assets/Sources/MazeMap";
+    public const string MazeMapInactiveFileName = "Assets/Sources/MazeMap";
+    public const string MazeMapActiveFileName = "Assets/Sources/NodeAndCharactersMap";
     public const int MazeWidth = 44;
     public const int MazeHeight = 20;
 
